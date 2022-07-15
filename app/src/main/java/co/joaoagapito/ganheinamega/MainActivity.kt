35d3649756c84c8b00bebed8a105f3f7fea2f17b
João Victor Agapito <55551801+JoaoAgapito@users.noreply.github.com>
2022-07-15 07:20:38 -0300

    refatorando

--- app/src/main/java/co/joaoagapito/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/joaoagapito/ganheinamega/MainActivity.kt
@@ -20,16 +20,6 @@ class MainActivity : AppCompatActivity() {
         val txtResult: TextView = findViewById(R.id.txt_result)
         val btnGenerate: Button = findViewById(R.id.btn_generate)
 
-
-        //CLICK - OPÇÃO 1: USANDO O XML: (obrigatorio declara um onClick no objeto)
-        // fun buttonClicked (view: View){}
-
-        //CLICK - OPÇÃO 2: VARIAVEL QUE SEJA DO TIPO View.OnClickListener (interface)
-        //btnGenerate.setOnClickListener(buttonClickListener)
-        //val buttonClickListener = object : View.OnClickListener{
-        //override fun onClick(p0: View?) {}
-
-        //CLICK - OPÇÃO 3: DECLARAR NO BLOCO DE CODIGO O QUE SERÁ DISPARADO PELO onClickListener
         btnGenerate.setOnClickListener {
             val text = editText.text.toString()
             numberGenerator(text, txtResult)
@@ -39,29 +29,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun numberGenerator(text: String, txtResult: TextView) {
         //validação para saber se o campo foi preenchido
-        if (text.isNotEmpty()) {
-            val qtd = text.toInt() // metodo para converter string em inteiro
-            if (qtd >= 6 && qtd <= 15) {
-
-                val numbers = mutableSetOf<Int>()
-                val random = Random
-
-                while (true) {
-                    val number = random.nextInt(60)
-                    numbers.add(number + 1)
-
-                    if (numbers.size == qtd) {
-                        break
-                    }
-                }
-                txtResult.text = numbers.joinToString(" - ")
-
-            } else
-                Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
-        } else {
+        if (text.isEmpty()) {
             Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
         }
+        val qtd = text.toInt() // metodo para converter string em inteiro
 
+        if (qtd < 6 || qtd > 15) {
+            Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+        val numbers = mutableSetOf<Int>()
+        val random = Random
+
+        while (true) {
+            val number = random.nextInt(60)
+            numbers.add(number + 1)
+
+            if (numbers.size == qtd) {
+                break
+            }
+        }
+        txtResult.text = numbers.joinToString(" - ")
 
     }
 
