3dd19f8322bd88d3bde4881bdee5bdf16b332601
João Victor Agapito <55551801+JoaoAgapito@users.noreply.github.com>
2022-07-16 07:41:28 -0300

    db

--- app/src/main/java/co/joaoagapito/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/joaoagapito/ganheinamega/MainActivity.kt
@@ -1,24 +1,40 @@
 package co.joaoagapito.ganheinamega
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.log
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var prefts: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // DECIDE O QUE O APP VAI FAZER (QUAL ATIVIDADE IRA ABRIR)
         setContentView(R.layout.activity_main)
 
+
         // BUSCAR OS OBJETOS E TER REFERECIA DELES:
         val editText: EditText = findViewById(R.id.edit_number)
         val txtResult: TextView = findViewById(R.id.txt_result)
         val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        // banco de dados de preferencia
+        prefts = getSharedPreferences("db", Context.MODE_PRIVATE)
+
+        val result = prefts.getString("result", null)
+        if (result != null){
+            txtResult.text = "Ultima aposta: $result"
+        }
 
         btnGenerate.setOnClickListener {
             val text = editText.text.toString()
@@ -51,6 +67,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
         txtResult.text = numbers.joinToString(" - ")
+
+        val editor = prefts.edit()
+        editor.putString("result", txtResult.text.toString())
+        val saved = editor.commit()
+        Log.i("teste","foi gravado $saved")
+
 
     }
 
